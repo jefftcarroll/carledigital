f38800da791bfc73017767e046afbde70a020f69
Mark Tellez <mark.tellez@icloud.com>
2019-08-13 15:49:01 -0500

    filtering plans based on querystring

--- src/plan-select/index.jsx
+++ src/plan-select/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
 import Grid from '@material-ui/core/grid'
+import queryString from 'query-string'
 
 import Plan from './plan'
 
 import plans from '../data/plans.json'
 
-export default function PlanSelect() {
+export default function PlanSelect({}) {
+  function filterPlans(plans) {
+    const keys = ['plan1', 'plan2', 'plan3']
+    const selections = queryString.parse(window.location.search)
+    const selectedPlans = keys.map(key => selections[key])
+
+    return plans.filter(plan => selectedPlans.includes(plan.ID))
+  }
   return (
     <Grid container direction="row" justify="space-evenly" alignItems="stretch">
-      {plans.map(json => (
+      {filterPlans(plans).map(json => (
         <Plan key={json.name} {...json} />
       ))}
     </Grid>
