f233e24844fa4c3a59426090f8d9c845afc61289
Mark Tellez <mark.tellez@icloud.com>
2019-08-13 17:03:00 -0500

    some basic theming and plan info

--- src/plan-select/index.jsx
+++ src/plan-select/index.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 
-import Grid from '@material-ui/core/grid'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 import queryString from 'query-string'
 
 import Plan from './plan'
 
 import plans from '../data/plans.json'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+  },
+  primaryButton: {
+    backgroundColor: '#572c80',
+  },
+  listHeadingContainer: {
+    height: 40,
+    position: 'fixed',
+    top: 0,
+    zIndex: 100,
+  },
+  listItemContainer: {
+    marginTop: 40,
+    width: '100%',
+    position: 'relative',
+    zIndex: 1,
+  },
+}))
+
 export default function PlanSelect({}) {
+  const classes = useStyles()
+
   function filterPlans(plans) {
     const keys = ['plan1', 'plan2', 'plan3']
     const selections = queryString.parse(window.location.search)
@@ -16,10 +43,37 @@ export default function PlanSelect({}) {
     return plans.filter(plan => selectedPlans.includes(plan.ID))
   }
   return (
-    <Grid container direction="row" justify="space-evenly" alignItems="stretch">
-      {filterPlans(plans).map(json => (
-        <Plan key={json.name} {...json} />
-      ))}
-    </Grid>
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+        className={classes.listHeadingContainer}
+      >
+        {filterPlans(plans).map(json => (
+          <Grid item key={json.ID} xs={12} sm={6} md={4}>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              className={classes.primaryButton}
+            >
+              {json.NAME}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
+
+      <div className={classes.listItemContainer}>
+        <Grid container className={classes.listItems}>
+          {filterPlans(plans).map(json => (
+            <Grid item key={json.name} xs={12} sm={6} md={4}>
+              <Plan {...json} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </div>
   )
 }
--- src/plan-select/plan/index.jsx
+++ src/plan-select/plan/index.jsx
@@ -1,5 +1,48 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
 
 export default function Plan(props) {
-  return <div>{props.NAME}</div>
+  return (
+    <Paper>
+      <h2>{props.NAME}</h2>
+      <h3>
+        {props.SUBNAME} ${props.PREMIUM}/month
+      </h3>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </Paper>
+  )
 }
